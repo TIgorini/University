c8b131e8ccd3c49dfc3ddc0ae9bb00907c5ff3a7
Igor Tymoshenko <mr7igrik@gmail.com>
2017-03-18 18:26:05 +0200

    Update laba5.asm

--- 4 cеместр/Assembly/Laba5/laba5.asm
+++ 4 cеместр/Assembly/Laba5/laba5.asm
@@ -12,29 +12,29 @@ click proc far
 	push   	es
 	pusha  
 	push   	0b800h
-	pop		es
+	pop	es
 
-	cmp		bx, 1		;button clicked: right or left
-	je		left
+	cmp	bx, 1		;button clicked: right or left
+	je	left
 	mov 	bx, 0
-	jmp		row_loop 
+	jmp	row_loop 
 left:
 	mov 	bx, 1
 row_loop:	
 	mov 	ax, bx
 	imul	ax, 80*2
-	mov		bp, ax
+	mov	bp, ax
 	mov 	di, 1
 	col_loop:
 		mov 	al, byte ptr es:[bp+di]
 		xor 	al, 00001100b
 		mov 	es:[bp+di], al
-		add		di, 2
-		cmp		di, 80*2
-		jb 		col_loop
+		add	di, 2
+		cmp	di, 80*2
+		jb 	col_loop
 	add 	bx, 2
-	cmp		bx, 24
-	jb		row_loop
+	cmp	bx, 24
+	jb	row_loop
 
 	popa
 	pop 	es
@@ -44,9 +44,9 @@ click endp
 
 start:
 	mov 	ax, data
-	mov		ds, ax
+	mov	ds, ax
 		 
-	mov	    ax, 3		;start text mode
+	mov	ax, 3		;start text mode
 	int 	10h
 
 	push 	es
@@ -54,37 +54,37 @@ start:
 	pop 	es
 	mov 	bx, 0
 write:	
-	mov		si, offset value
+	mov	si, offset value
 	mov 	ax, bx
 	imul	ax, 80*2
-	mov		di, ax
+	mov	di, ax
 	mov 	cx, count
 	rep 	movsw
 	inc 	bx
-	cmp		bx, 24
-	jb		write
+	cmp	bx, 24
+	jb	write
 	pop 	es
 	
 	xor 	ax, ax		;start mouse handle
 	int 	33h
-	mov		ax, 0ch		
-	mov		cx, 1010b
+	mov	ax, 0ch		
+	mov	cx, 1010b
 	push 	es
 	push 	cs
 	pop 	es
 	lea 	dx, click
-	int		33h
+	int	33h
 	pop 	es
 
 	mov 	ah, 01h		;waiting for key press	
-	int		21h		 
+	int	21h		 
 
 	xor 	cx,cx		;stop mouse handle
-	mov		ax,0ch		
-	int		33h
+	mov	ax,0ch		
+	int	33h
 
-	mov		ax, 4c00h
-	int		21h
+	mov	ax, 4c00h
+	int	21h
 
 code ends
-end	start	
+end start	
