bd34c0488ec2db52983c4b2b477d974e9e30d8ad
tigorini <mr7igrik@gmail.com>
2017-05-13 19:36:38 +0300

    PRO laba4 added

--- 4 cеместр/ПРО/Laba4/laba4.cpp
+++ 4 cеместр/ПРО/Laba4/laba4.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
 #include "CyclicBuffer.h"
-
-using namespace std;
 
 FILE *log;
 CyclicBuffer* cr;
@@ -30,40 +27,29 @@ void* prodFunc(void* arg){
 	while (1){
 
 		sem_getvalue(&sem_cr, &sem_val);
-		if (sem_val == 0)
-			break;
+		if (sem_val < SIZE - 1){
+						
+			while (pthread_mutex_trylock(&mut_cr) != 0){
+				//do some useful work
+			}
 
-		sem_getvalue(&sem_cr, &sem_val);
-		if (sem_val < SIZE - 2){
+			fprintf(log, "\tP%d lock mutex\n", curr);
+			fprintf(log,"P%d put %d\n",curr, cr->put());
+			pthread_mutex_unlock(&mut_cr);
+			fprintf(log, "\tP%d unlock mutex\n", curr);
 			
-			fprintf(log, "\tP%d try lock mutex\n", curr);			
-			if (pthread_mutex_trylock(&mut_cr) == 0){
-			
-				//fprintf(log, "\tP%d lock mutex\n", curr);
-				fprintf(log,"P%d put %d\n",curr, cr->put());
-				//fprintf(log, "\tP%d unlock mutex\n", curr);
-				pthread_mutex_unlock(&mut_cr);
-				sem_post(&sem_cr);
-				//sem_getvalue(&sem_cr, &sem_val);
-				//fprintf(log, "\t\tP%d inc sem (%d)\n", curr, sem_val);
-			}else
-				fprintf(log,"\tP%d do some work\n",curr);
-		}		
-		
+			sem_post(&sem_cr);
+		}
+		usleep(1);		
 	}
 
-	pthread_cancel(c1);
-	pthread_cancel(c2);
-	fprintf(log, "P%d stoped\n", curr);	
 	return NULL;
 } 
 
 
 void* consumFunc1(void* no_arg){
 
-	int sem_val; 
 	fprintf(log, "C1 started\n");
-
 	while (1){
 
 		fprintf(log,"\tC1 send to C2\n");
@@ -72,63 +58,57 @@ void* consumFunc1(void* no_arg){
 		sem_wait(&sem1);
 
 		if (sem_trywait(&sem_cr) == 0){
-			//sem_getvalue(&sem_cr, &sem_val);
-			//fprintf(log,"\t\tC1 dec sem (%d)\n",sem_val);
 
-			//fprintf(log,"\tC1 try lock mutex\n");
-			if (pthread_mutex_trylock(&mut_cr) == 0){
-
-				//fprintf(log,"\tC1 lock mutex\n");
-				fprintf(log,"C1 get %d\n", cr->get());
-				pthread_mutex_unlock(&mut_cr);
-				//fprintf(log,"\tC1 unlock mutex\n");
+			while (pthread_mutex_trylock(&mut_cr) != 0){
+				//do some useful work
 			}
-			else
-				fprintf(log,"\tC1 do some work\n");
+
+			fprintf(log,"\tC1 lock mutex\n");
+			fprintf(log,"C1 get %d\n", cr->get());
+			pthread_mutex_unlock(&mut_cr);
+			fprintf(log,"\tC1 unlock mutex\n");
 
 		}else
 			break;
 	}
 
-	fprintf(log, "C1 stoped\n");
+	pthread_cancel(p1);
+	pthread_cancel(p2);
+	pthread_cancel(p3);
 	return NULL;
 }
 
 
 void* consumFunc2(void* no_arg){
 
-	int sem_val; 
 	fprintf(log, "C2 started\n");
-
 	while (1){
 
 		fprintf(log,"\tC2 send to C1\n");
 		sem_post(&sem1);
 		fprintf(log,"\tC2 try to wait C1\n");
-		if (sem_trywait(&sem2) == 0){
+		while (sem_trywait(&sem2) != 0){
+			//do some useful work
+		}
 
-			if (sem_trywait(&sem_cr) == 0){
-				//sem_getvalue(&sem_cr, &sem_val);
-				//fprintf(log,"\t\tC2 dec sem (%d)\n", sem_val);
+		if (sem_trywait(&sem_cr) == 0){
 
-				fprintf(log,"\tC2 try lock mutex\n");
-				if (pthread_mutex_trylock(&mut_cr) == 0){
+			while (pthread_mutex_trylock(&mut_cr) != 0){
+				//do some useful work
+			}
 
-					//fprintf(log,"\tC2 lock mutex\n");
-					fprintf(log,"C2 get %d\n", cr->get());
-					pthread_mutex_unlock(&mut_cr);
-					//fprintf(log,"\tC2 unlock mutex\n");
-				}
-				else
-					fprintf(log,"\tC2 do some work\n");
+			fprintf(log,"\tC2 lock mutex\n");
+			fprintf(log,"C2 get %d\n", cr->get());
+			pthread_mutex_unlock(&mut_cr);
+			fprintf(log,"\tC2 unlock mutex\n");	
 			
-			}else
-				break;
 		}else
-			fprintf(log,"\tC2 do some work\n");			
+			break;			
 	}
-
-	fprintf(log, "C2 stoped\n");
+ 	
+ 	pthread_cancel(p1);
+	pthread_cancel(p2);
+	pthread_cancel(p3);
 	return NULL;
 }
 
@@ -143,12 +123,6 @@ int main(){
 	sem_init(&sem2,0,0);
 	cr = new CyclicBuffer();
 
-	for (int i = 0; i < 3; i++){
-		cr->put();
-		sem_post(&sem_cr);
-	}
-	fprintf(log, "Created first 3 elements of CR: 1, 2, 3\n");
-
 	int id_p1 = 1;
 	int id_p2 = 2;
 	int id_p3 = 3;
@@ -159,11 +133,11 @@ int main(){
 	pthread_create(&c1, NULL, &consumFunc1, NULL); 
 	pthread_create(&c2, NULL, &consumFunc2, NULL);
 
-	pthread_join(p1, NULL);
-	pthread_join(p2, NULL);
-	pthread_join(p3, NULL);
-	pthread_join(c1, NULL);
-	pthread_join(c2, NULL);
+	pthread_join(p1, NULL); 	fprintf(log, "P1 stoped\n");
+	pthread_join(p2, NULL); 	fprintf(log, "P2 stoped\n");
+	pthread_join(p3, NULL); 	fprintf(log, "P3 stoped\n");
+	pthread_join(c1, NULL);		fprintf(log, "C1 stoped\n");
+	pthread_join(c2, NULL);		fprintf(log, "C2 stoped\n");
 
 	delete cr;
 	fprintf(log, "Main stoped\n");
