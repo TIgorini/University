57e46c7abaccb69c1a9376452ff94c195a2acdbf
tigorini <mr7igrik@gmail.com>
2017-10-17 09:42:21 +0300

    CALaba1 corrected so so right

--- term_5/CompArch/Laba1/sample/Controller.java
+++ term_5/CompArch/Laba1/sample/Controller.java
@@ -1,10 +1,8 @@
 package sample;
 
-import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.*;
-import javafx.util.Duration;
 
 public class Controller {
     @FXML private Line ready;
@@ -12,47 +10,37 @@ public class Controller {
     @FXML private Circle lamp;
 
     private int signal = 0;
-    private Runnable startR = new Runnable() {
+    private Runnable loopRunnable = new Runnable() {
         @Override
         public void run() {
-            signal = 1;
-            ready.setStroke(Paint.valueOf("#cc0000"));
-            try {
-                Thread.sleep(1000);
-                if (signal == 1) {
-                    control.setStroke(Paint.valueOf("#cc0000"));
-                    //Thread.sleep(1000);
-                    lamp.setFill(Paint.valueOf("#EFF123"));
+            while (true){
+                while (signal == 1){
+                    try {
+                        control.setStroke(Paint.valueOf("#cc0000"));
+                        lamp.setFill(Paint.valueOf("#EFF123"));
+                        Thread.sleep(1000);
+                        control.setStroke(Paint.valueOf("#000000"));
+                        lamp.setFill(Paint.valueOf("#FFFFFF"));
+                        Thread.sleep(1000);
+                    } catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
     };
-    private Runnable stopR = new Runnable() {
-        @Override
-        public void run() {
-            signal = 0;
-            ready.setStroke(Paint.valueOf("#000000"));
-            try {
-                Thread.sleep(1000);
-                if (signal == 0) {
-                    control.setStroke(Paint.valueOf("#000000"));
-                    lamp.setFill(Paint.valueOf("#FFFFFF"));
-                }
-            } catch(InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-    };
+    private Thread loopThread = new Thread(loopRunnable);
 
     @FXML protected void startSignal(){
-        Thread startThread = new Thread(startR);
-        startThread.start();
+        if (!loopThread.isAlive()){
+            loopThread.start();
+        }
+        ready.setStroke(Paint.valueOf("#cc0000"));
+        signal = 1;
     }
 
     @FXML protected void stopSignal(){
-        Thread stopThread = new Thread(stopR);
-        stopThread.start();
+        ready.setStroke(Paint.valueOf("#000000"));
+        signal = 0;
     }
 }
