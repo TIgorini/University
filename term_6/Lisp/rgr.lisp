c68b157297e4a030bb0da304af6a46d66dd377df
tigorini <mr7igrik@gmail.com>
2018-03-19 20:49:25 +0200

    RGR lisp added

---
+++ term_6/Lisp/rgr.lisp
@@ -0,0 +1,29 @@
+(defun f1(i)
+	(if (eql i 1) 1
+		(- i (sin (+ (f1 (- i 1)) i)))
+	)
+)
+
+(defun f2(i)
+	(if (eql i 10) 1
+		(* 4 (cos (* (f2 (- i 1)) (log i))))
+	)
+)
+
+(defun f(i)
+	(cond ((< i 1) nil)
+		((< i 10) (f1 i))
+		((< i 21) (f2 i))
+		(T nil)
+	)
+)
+
+;tests
+(print (f -5))
+(print (f 1))
+(print (f 3))
+;(print (- 3 (sin (+ (- 2 (sin (+ 1 2))) 3))))	
+(print (f 10))
+(print (f 12))
+(print (f 21))
+(terpri)
