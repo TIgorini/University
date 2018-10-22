6682eada1a03e3730e2682eb90b3b4fcc7101c4d
tigorini <mr7igrik@gmail.com>
2018-10-22 17:26:38 +0300

    OS Laba4 almost done

---
+++ term_7/Operation_Systems/Laba4/avl_tree.py
@@ -0,0 +1,198 @@
+class tree_node:
+    """Node for AVL tree"""
+
+    def __init__(self, value):
+        self.val = value
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.hl = 0
+        self.hr = 0
+
+    def __str__(self):
+        return str(self.val)
+
+
+class avl_tree:
+    """Realisation of AVL tree"""
+
+    def __init__(self, list=[]):
+        self._root = None
+        for val in list:
+            self.add(val)
+
+    def _turn_right(self, node):
+        """Provides Right and LeftRight rotations
+
+        Returns node that will be on place of previos
+        node
+        """
+        if node.left.hr > node.left.hl:
+            buf = node.left
+            node.left = buf.right
+            node.left.parent = node
+            buf.right = node.left.left
+            node.left.left = buf
+            buf.parent = node.left
+
+            buf.hr = node.left.hl
+            node.left.hl = max(buf.hl, buf.hr) + 1
+
+        parent = node.parent
+        buf = node.left
+
+        node.left = buf.right
+        buf.right = node
+        node.parent = buf
+
+        node.hl = buf.hr
+        buf.hr = max(node.hl, node.hr) + 1
+
+        if parent is None:
+            self._root = buf
+            buf.parent = None
+        elif parent.left is node:
+            parent.left = buf
+            buf.parent = parent
+        else:
+            parent.right = buf
+            buf.parent = parent
+        return buf
+
+    def _turn_left(self, node):
+        """Provides Left and RightLeft rotations
+
+        Returns node that will be on place of previos
+        node
+        """
+        if node.right.hl > node.right.hr:
+            buf = node.right
+            node.right = buf.left
+            node.right.parent = node
+            buf.left = node.right.right
+            node.right.right = buf
+            buf.parent = node.right
+
+            buf.hl = node.right.hr
+            node.right.hr = max(buf.hl, buf.hr) + 1
+
+        parent = node.parent
+        buf = node.right
+
+        node.right = buf.left
+        buf.left = node
+        node.parent = buf
+
+        node.hr = buf.hl
+        buf.hl = max(node.hl, node.hr) + 1
+
+        if parent is None:
+            self._root = buf
+            buf.parent = None
+        elif parent.left is node:
+            parent.left = buf
+            buf.parent = parent
+        else:
+            parent.right = buf
+            buf.parent = parent
+        return buf
+
+    def _add_to_node(self, val, node):
+        """Recursive function that search place to put element val,
+        adds element after finding a place and provides rebalancing
+        if necessary.
+
+        Returns height of this branch
+        """
+        if val <= node.val:
+            if node.left is None:
+                new_node = tree_node(val)
+                new_node.parent = node
+                node.left = new_node
+                node.hl += 1
+            else:
+                node.hl = self._add_to_node(val, node.left)
+                if node.hl - node.hr == 2:
+                    node = self._turn_right(node)
+        else:
+            if node.right is None:
+                new_node = tree_node(val)
+                new_node.parent = node
+                node.right = new_node
+                node.hr += 1
+            else:
+                node.hr = self._add_to_node(val, node.right)
+                if node.hr - node.hl == 2:
+                    node = self._turn_left(node)
+        return max(node.hr, node.hl) + 1
+
+    def add(self, val):
+        """Adds element val to tree if it's not already in tree"""
+        if self.search(val):
+            return False
+
+        if self._root is None:
+            self._root = tree_node(val)
+        else:
+            self._add_to_node(val, self._root)
+        return True
+
+    def _compare_to_node(self, val, node):
+        """Recursive function of search"""
+        if node is None:
+            return False
+        elif val == node.val:
+            return True
+        elif val <= node.val:
+            return self._compare_to_node(val, node.left)
+        else:
+            return self._compare_to_node(val, node.right)
+
+    def search(self, val):
+        """Returns True if element val founded, otherwise returns False"""
+        return self._compare_to_node(val, self._root)
+
+    def _make_str(self, node, tab):
+        """Recursive function to collect readable representation of tree"""
+        if node is None:
+            return '{:{fill}<{tab}}None\n'.format('', fill='.', tab=tab)
+        return '{:{fill}<{tab}}{}({}, {})\n'.format(
+            '', node.val, node.hl, node.hr, fill='.', tab=tab) +\
+            self._make_str(node.left, tab + 2) +\
+            self._make_str(node.right, tab + 2)
+
+    def __str__(self):
+        return self._make_str(self._root, 0)
+
+
+if __name__ == '__main__':
+    tree = avl_tree(['a8', 'b7', 'a1'])
+    print('Initial tree:')
+    print(tree)
+    print('\nCommands:\n \
+show\n \
+add [element]\n \
+remove [element]\n \
+search [element]\n \
+exit')
+
+    while True:
+        com = input('\n> ')
+        print('')
+        args = com.split(' ')
+        if args[0] == 'show':
+            print(tree)
+        elif args[0] == 'add':
+            if not tree.add(args[1]):
+                print('Element already exist')
+            else:
+                print('Success.\n')
+                print(tree)
+        elif args[0] == 'search':
+            print(tree.search(args[1]))
+        elif args[0] == 'remove':
+            print('Not found')
+        elif args[0] == 'exit':
+            break
+        else:
+            print('\nCommands:\n add [element]\n show\n exit')
--- term_7/Operation_Systems/README.md
+++ term_7/Operation_Systems/README.md
@@ -4,4 +4,4 @@
 * [Laba1. Process planning](https://github.com/TIgorini/University/tree/master/term_7/Operation_Systems/Laba1) 
 * [Laba2. Process synchronization](https://github.com/TIgorini/University/tree/master/term_7/Operation_Systems/Laba2)
 * Laba3. Memory management 
-* Laba4. File systems
+* [Laba4. File systems](https://github.com/TIgorini/University/tree/master/term_7/Operation_Systems/Laba4)
