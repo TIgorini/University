f95be27da4cfc48ae94ab20398e7b49a0f94693b
tigorini <mr7igrik@gmail.com>
2018-11-08 14:43:21 +0200

    OS laba3 is done

--- term_7/Operation_Systems/Laba3/mem_manager.py
+++ term_7/Operation_Systems/Laba3/mem_manager.py
@@ -16,12 +16,22 @@ class Process:
                             'partition': None})
             self.vm_size += self.vm[i]['size']
 
+    def read(self, seg, addr):
+        if seg > len(self.vm) - 1 or addr > self.vm_size - 1:
+            return -1
+        else:
+            if self.vm[seg]['in_mem'] == 0:
+                mem.upload(self.vm, self.vm_size, self.id)
+            return self.vm[seg]['addr'] + addr
+
     def __str__(self):
-        res = f'   Process {self.id}\nVM size: {self.vm_size}\n'
+        res = '--------------------Process-{}--------------------\n'.\
+            format(self.id)
+        res += 'Size: {}\n'.format(self.vm_size)
         for seg in self.vm:
             res += 'Segment {}:\n\tAddress: {}\n\tSize: {}\n\tIn memory: {}\n'\
                 .format(seg['id'], seg['addr'], seg['size'], seg['in_mem'])
-        return res
+        return res + '-------------------------------------------------\n'
 
 
 class Memory:
@@ -45,12 +55,12 @@ class Memory:
                                 'free': 249,
                                 'nfree': 0,
                                 'proc': None})
-        self.part_table.append({'id': 3,
-                                'addr': 724,
-                                'size': 300,
-                                'free': 300,
-                                'nfree': 0,
-                                'proc': None})
+        # self.part_table.append({'id': 3,
+        #                         'addr': 724,
+        #                         'size': 300,
+        #                         'free': 300,
+        #                         'nfree': 0,
+        #                         'proc': None})
 
     def upload(self, segments, size, pid):
         '''Uploads proccess segments to memory'''
@@ -93,7 +103,10 @@ class Memory:
         """Unloads proccess segments from memory"""
         rev_segments = reversed(segments)
         for seg in rev_segments:
-            part = self.part_table[seg['partition']]
+            part_id = seg['partition']
+            if part_id is None:
+                continue
+            part = self.part_table[part_id]
             seg['addr'] = 0
             seg['in_mem'] = 0
             seg['partition'] = None
@@ -115,7 +128,7 @@ class Memory:
         part['free'] = part['size']
 
     def __str__(self):
-        res = ''
+        res = '---------------------Memory---------------------\n'
         for part in self.part_table:
             res += 'Partition {}: (Size: {}, Free: {}'.\
                 format(part['id'], part['size'], part['free'])
@@ -131,25 +144,75 @@ class Memory:
                             format(seg['addr'], seg['id'], seg['addr'] + seg['size'] - 1)
                 res += '  {}\n  .\n  .\tfree\n  .\n  {}\n'.\
                     format(part['addr'] + part['nfree'], part['addr'] + part['size'] - 1)
-        return res
+        return res + '-------------------------------------------------\n'
+
+
+def print_commands():
+    print('Commands:\n\
+  1. upload [pid]\n\
+  2. unload [pid]\n\
+  3. show [pid]\n\
+  4. read [pid]\n\
+  5. show\n\
+  6. help\n\
+  7. exit\n')
 
 
 if __name__ == '__main__':
     mem = Memory()
-    print('---------------------------------------------')
     print(mem)
-    print('---------------------------------------------')
     processes = []
-    print('---------------------------------------------')
-    for i in range(3):
+    for i in range(4):
         processes.append(Process(i))
         print(processes[i])
-    print('---------------------------------------------')
+
+    print_commands()
 
     while True:
-        pid = int(input('Enter pid to upload: '))
-        proc = processes[pid]
-        mem.upload(proc.vm, proc.vm_size, pid)
-        print('---------------------------------------------')
-        print(mem)
-        print('---------------------------------------------')
+        com = input('> ')
+        com = com.split(' ')
+        if len(com) == 1:
+            if com[0] == 'show':
+                print(mem)
+            elif com[0] == 'exit':
+                break
+            else:
+                print_commands()
+
+        elif len(com) == 2:
+            try:
+                pid = int(com[1])
+            except ():
+                print_commands()
+                continue
+
+            if pid > len(processes) - 1:
+                print('pid must be < {}'.format(len(processes) - 1))
+            elif com[0] == 'upload':
+                proc = processes[pid]
+                mem.upload(proc.vm, proc.vm_size, proc.id)
+                print(mem)
+            elif com[0] == 'unload':
+                proc = processes[pid]
+                mem.unload(proc.vm)
+                print(mem)
+            elif com[0] == 'read':
+                proc = processes[pid]
+                print(proc)
+                print('Choose address to read like: [seg] [addr]\n')
+                com = input('> ')
+                com = com.split(' ')
+                seg = int(com[0])
+                vm_addr = int(com[1])
+                addr = proc.read(seg, vm_addr)
+                if addr == -1:
+                    print('No such address')
+                else:
+                    print('Virtual memory address: ({}, {})'.format(seg, vm_addr))
+                    print('Real memory address: {}\n'.format(addr))
+            elif com[0] == 'show':
+                print(processes[pid])
+            else:
+                print_commands()
+        else:
+            print_commands()
